bb12b1ffc5425e252803708608ee17f3d925d071
Steve Gardner <gardnersj@gmail.com>
2015-09-07 13:09:51 -0400

    reset and back button functionality

--- quiz-ui.js
+++ quiz-ui.js
@@ -1,8 +1,146 @@
 var QuizUI = {
-  nextButton: document.getElementByID( 'next' ),
-  quiz: document.getElementById( 'quiz' ),
-  selection: document.getElementById( 'input' ),
-  quizContainer: document.getElementById( 'quiz-container' ),
-  resetButton: document.getElementById( 'reset' ),
+  nextButton: document.getElementById('next'),
+  backButton: document.getElementById('back'),
+  resetButton: document.getElementById( 'reset' ), 
+
+  displayNext: function() {
+    // Determine if quiz is over
+    if (quiz.hasEnded()) {
+      this.endDisplay();
+      this.showReset();
+    } else {
+      this.displayQuestionHeading();
+      this.displayQuestion();
+      this.displayChoices();
+      this.clearClass('checked');
+      this.showBackButton();
+      this.nextButton.disabled = true;
+    }
+  }, 
   
-}
+  displayQuestionHeading: function() {
+    var heading = 'Question ' + (quiz.currentQuestion + 1);
+    this.populateHTML('question-heading', heading)
+  },
+  
+  displayQuestion: function() {
+    var question = quiz.questions[quiz.currentQuestion].question;
+    this.populateHTML('question', question);
+  },
+  
+  displayChoices: function() {
+    var i = 0,
+        end = quiz.questions.length,
+        choices;
+    
+    for (i; i < end; i += 1) {
+      choices = quiz.getCurrentQuestion().choices;
+      this.populateHTML('choice' + i, choices[i]);
+      this.guessHandler('choice' + i, choices[i]);
+    }
+  },
+  
+  endDisplay: function() {
+    var thanks = "Congratulations! You answered " + quiz.correctAnswers + " questions correctly. <br>Your score is " + quiz.score,
+        quizBody = document.getElementById('quiz'),
+        thanksDisplay = document.getElementById('thanks');
+    
+    quizBody.style.display = 'none';
+    thanksDisplay.style.display = 'block';
+    QuizUI.populateHTML('thanks', thanks);
+  },
+  
+  /* Quiz Handlers
+  *******************************************/
+  guessHandler: function(id, guess) {
+    var choice = document.getElementById(id);
+    choice.onclick = function(e) {
+      QuizUI.addCheckedToAnswer.call(this, 'checked');  
+      QuizUI.nextButton.disabled = false;
+      QuizUI.submitHandler(id, guess);
+    };
+  },
+  
+  submitHandler: function(id, guess) {
+    this.nextButton.onclick = function(e) {
+      e.preventDefault();
+      quiz.guess(id, guess);
+      quiz.checkAnswers();
+      QuizUI.displayNext();
+    }
+  },
+  
+  backHandler: function() {
+    this.backButton.onclick = function(e) {
+      e.preventDefault();
+      quiz.getLastQuestion();
+      QuizUI.displayNext();
+      QuizUI.setPreviousAnswer(quiz.getLastGuess());
+      quiz.removeGuess();
+    }
+  },
+  
+  showBackButton: function() {
+    if (quiz.currentQuestion) {
+      this.backButton.disabled = false;
+      this.backHandler();
+    } else {
+      this.backButton.disabled = true;
+    }
+  },
+  
+  /* Checked Class Manipulators
+  **********************************************/
+  addCheckedToAnswer: function(className) {
+    var i = 0;
+    var answers = document.getElementsByClassName('answer');
+    for (; i < 4; i += 1) {
+      answers[i].classList.remove(className);
+    }
+    this.classList.add(className);
+  },
+  
+  clearClass: function(className) {
+    var elements = document.getElementsByClassName(className);
+    while (elements.length) elements[0].classList.remove(className);
+  },
+  
+  setPreviousAnswer: function(id) {
+    var guess = document.getElementById(id);
+    guess.classList.add('checked');
+  },
+  
+  
+  
+/* Resets
+  **********************************************/
+  
+  resetHandler: function() {
+    var thanksDisplay = document.getElementById('thanks'),
+        quizBody = document.getElementById('quiz');
+    
+    thanksDisplay.style.display = 'none';
+    quizBody.style.display = 'block';
+    QuizUI.resetButton.style.display = 'none';
+    QuizUI.nextButton.disabled = true;
+    quiz.currentQuestion = 0;
+    quiz.score = 0;
+    quiz.correctAnswers = 0; 
+    quiz.guesses = [];
+    QuizUI.displayNext();
+  },  
+  
+  showReset: function() {
+    this.resetButton.style.display = 'block';
+    this.resetButton.onclick = this.resetHandler;
+  },
+  
+  
+  /* Helpers
+  **********************************************/
+  
+  populateHTML: function(id, data) {
+    var element = document.getElementById(id);
+    element.innerHTML = data;
+  }
+};
--- quiz.js
+++ quiz.js
@@ -1,10 +1,62 @@
 function Quiz(questions) {
+  this.questions = questions;
   this.currentQuestion = 0;
-  this.correct = '';
-  this.addToScore = 0;
-  this.correctAnsers = 0;
-  this.score = 0;
-  
-  
-    
+  this.correctAnswers = 0;
+  this.score = 0; 
+  this.guesses = [];
+};
+
+Quiz.prototype.getNextQuestion = function () {
+  return this.currentQuestion += 1; 
+};
+
+Quiz.prototype.getLastQuestion = function() {
+  return this.currentQuestion -= 1;
 }
+
+Quiz.prototype.guess = function(id, guess) {
+  this.currentQuestion += 1; 
+  this.addToGuesses(id);
+};
+
+// Check answers
+Quiz.prototype.checkAnswers = function() {
+  var i = 0,
+      end = this.guesses.length;
+  this.score = 0;
+  this.correctAnswers = 0;
+  
+  for (; i < end; i++) {
+    if (this.questions[i].correctAnswer === this.guesses[i].text) {
+      this.score += 5;
+      this.correctAnswers += 1;
+    }
+  }
+}
+
+Quiz.prototype.hasEnded = function() {
+  return this.currentQuestion >= this.questions.length;
+};
+
+Quiz.prototype.getCurrentQuestion = function() {
+  return this.questions[this.currentQuestion];
+};
+
+Quiz.prototype.addToGuesses = function(id) {
+  var guess = {},
+      guessText = document.getElementById(id);
+  guess.id = id;
+  guess.text = guessText.innerHTML;
+  
+  this.guesses.push(guess);
+}
+
+Quiz.prototype.getLastGuess = function() {
+  return this.guesses[this.guesses.length - 1].id;
+}
+
+// Remove guess object from guesses array
+Quiz.prototype.removeGuess = function() {
+  this.guesses.pop();
+}
+
